88dd3da421125238d998d71a6a585b0473eb44d9
Shi Hao Wang <st890409@hotmail.com>
2020-05-23 09:14:52 +0800

    建立範例程式碼

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# SpringNetSampleApp
- 在 .NET Framework 使用 Spring.NET 套件範例
+
+# 使用 Spring 框架進行 Dependency Injection 的範例
+
+## 環境
+
+- .NET Framework 4.5.1
+- Spring.Core 2.0.1
+
+## 情境
+
+透過抽離 DateTime 介面依 App.config 進行不同的注入實作
+
+```
+<spring>
+	<context>
+		<resource uri="config://spring/objects"/>
+	</context>
+	<objects xmlns="http://www.springframework.net">
+		<!-- 取消註解來指定 IDateTime 介面的實作物件 -->
+		<!-- 
+		<object name="InjectDateTime" type="SpringNetSampleApp.BirthDateTime, SpringNetSampleApp"></object>
+		<object name="InjectDateTime" type="SpringNetSampleApp.UtcDateTime, SpringNetSampleApp"></object>
+		<object name="InjectDateTime" type="SpringNetSampleApp.LocalDateTime, SpringNetSampleApp"></object>
+		 -->
+	</objects>
+</spring>
+```
+
+執行後就可以看到主控台輸出結果
---
+++ SpringNetSampleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using Spring.Context;
+using Spring.Context.Support;
+using System;
+
+namespace SpringNetSampleApp
+{
+    class Program
+    {
+        const string springObjectName = "InjectDateTime";
+
+        static void Main(string[] args)
+        {
+            IApplicationContext _context;
+            IDateTime _dateTime;
+
+            _context = ContextRegistry.GetContext();
+            _dateTime = _context[springObjectName] as IDateTime;
+
+            Console.WriteLine("{0:yyyy-MM-dd HH:mm:ss}", _dateTime.Now());
+
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+
+    public interface IDateTime
+    {
+        DateTime Now();
+    }
+
+    public class LocalDateTime : IDateTime
+    {
+        public DateTime Now()
+        {
+            return DateTime.Now;
+        }
+    }
+
+    public class UtcDateTime : IDateTime
+    {
+        public DateTime Now()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+
+    public class BirthDateTime : IDateTime
+    {
+        public DateTime Now()
+        {
+            return Convert.ToDateTime("1988/01/18");
+        }
+    }
+}
